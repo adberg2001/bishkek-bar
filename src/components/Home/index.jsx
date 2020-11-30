9595d670055d424ed6bb9e5ef76c213fe37fba5c
aaaadyl <aaaadyl.erg@gmail.com>
2020-12-01 04:34:55 +0600

    style home title, fix style of drawer (change transition) + header offsetY transition

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import style from "./style.module.sass"
 import MainWrapper from "../MainWrapper/index";
+import Title from "./Title";
 
 function HomeComponent() {
 
     return (
         <MainWrapper>
             <div className={style.mainCont}>
-
+              <Title/>
             </div>
         </MainWrapper>
     )
--- src/components/MainWrapper/Header/index.jsx
+++ src/components/MainWrapper/Header/index.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from "./style.module.sass"
 import {NavLink} from "react-router-dom";
 import logo from "./assets/vigbarlogo.png"
-import { Sling as Hamburger } from 'hamburger-react'
+import {Sling as Hamburger} from 'hamburger-react'
 
 function Header({toggleDrawer, open}) {
+  const [yOffSet, setYOffSet] = useState(window.pageYOffset)
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll, false)
+    return document.addEventListener("scroll", handleScroll)
+  }, [])
 
+  const handleScroll = () => setYOffSet(window.pageYOffset);
+  console.log(yOffSet)
   return (
-    <header className={style.header}>
+    <header className={`${style.header} ${yOffSet < 150 ? style.black : style.transparent}`}>
       <NavLink to="/">
         <img src={logo} className={style.logo} alt=""/>
       </NavLink>
-      <span className={open?style.open:style.close}>
-        <Hamburger toggled={open} size={25} toggle={toggleDrawer(!open)} />
+      <span>
+        <Hamburger toggled={open} size={25} toggle={toggleDrawer(!open)}/>
       </span>
     </header>
   )
