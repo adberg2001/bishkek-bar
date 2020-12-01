ef660b94832d0193b274efab524eefb27fd6c69a
aaaadyl <aaaadyl.erg@gmail.com>
2020-12-02 00:10:18 +0600

    fix Drawer close btn, create Departments directory

---
+++ src/components/Departments/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./style.module.sass"
+
+function Departments(){
+
+  return (
+    <div className={style.mainCont}>
+
+    </div>
+  )
+}
+
+export default Departments;
